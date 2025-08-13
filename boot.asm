c7b1d406c606c5962a8d54f96c4c46a33e8fc107
ethanhzhang2-dotcom <ethanhzhang2@gmail.com>
2025-08-13 12:38:54 -0400

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -4,6 +4,13 @@ header_start:
   dd 0xe85250d6 ;magic number for GRUB multiboot
   dd 0; protected mode code: won't let us do anything dangerous 
   dd header_end- header_start; header length 
+    ; checksum
+    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+    ; required end tag
+    dw 0    ; type
+    dw 0    ; flags
+    dd 8    ; size
 header_end:
 
 
